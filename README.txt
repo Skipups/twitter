fdcca52df1c518b8e9d26eb36846862ea9d577d2
Skipups <svetlana.rovinsky@gmail.com>
2020-04-08 10:22:17 -0400

    needed to switch

--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
 a small twitter app
+
+1. app.js discuss order and what each line does
+2. discuss testing route in app.js and  users.js
+3. file tree
--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const chalk = require('chalk');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello'));
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
 
 mongoose
   .connect(process.env.MONGOURI_HOST, {
@@ -14,6 +16,18 @@ mongoose
   })
   .then(() => console.log(chalk.green('Connected to MongoDb successfully')))
   .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  console.log(res);
+  res.send('Hello');
+});
+
+//middleware for parsing json objects
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 app.listen(port, () =>
   console.log(chalk.blue(`Server is listening on ${port}`))
